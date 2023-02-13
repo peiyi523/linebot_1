de99be76c6655c5b0ab351351a36eca8d72baba8
peiyiwu <peiyi523@gmail.com>
2023-02-14 00:21:52 +0800

    初始環境設定

--- linebot1/urls.py
+++ linebot1/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import re_path as url
+#from django.conf.urls import url
+from bot import views
 
 urlpatterns = [
+    url('^callback',views.callback),
     path('admin/', admin.site.urls),
 ]
